5b4c50634f108f2b57e477a84186b3bd53e1c720
VishnuSanal <t.v.s10123@gmail.com>
2024-03-09 13:43:18 +0530

    use git global username as fallback

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use colored::*;
 use reqwest::header::USER_AGENT;
 use reqwest::Error;
@@ -29,8 +31,20 @@ struct Event {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args: Vec<_> = std::env::args().collect();
+
     if args.len() < 2 {
-        panic!("Please provide the username as a command line arguement!");
+        println!("No username provided, falling back to global Git username");
+
+        let output = Command::new("git config --global user.name")
+            .output()
+            .expect("No Git username set");
+
+        if output.status.success() {
+            let username = String::from_utf8(output.stdout);
+            println!("{:?}", username);
+        } else {
+            panic!("Please provide the username as a command line argument!");
+        }
     }
 
     let request_url = format!(
