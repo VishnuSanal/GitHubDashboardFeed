a8e2d4f327e23983d62c24bd31b4bcf4e134dc8b
VishnuSanal <t.v.s10123@gmail.com>
2024-03-11 20:47:22 +0530

    [WIP] new UI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::process::Command;
+/*use std::process::Command;
 
 use colored::*;
-use reqwest::header::USER_AGENT;
 use reqwest::Error;
+use reqwest::header::USER_AGENT;
 use serde::Deserialize;
 use terminal_link::Link;
+
+use std::io::{self, stdout};
+
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::*, widgets::*};
 
 #[derive(Deserialize, Debug)]
 struct Actor {
@@ -16,7 +25,7 @@ struct Actor {
 struct Repo {
     // id: u32,
     name: String,
-    // url: String,
+    url: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -26,6 +35,16 @@ struct Event {
     actor: Actor,
     repo: Repo,
     // created_at: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct RepoDetails {
+    // id: u32,
+    name: String,
+    description: Option<String>,
+    // html_url: String,
+    stargazers_count: i32,
+    language: Option<String>,
 }
 
 #[tokio::main]
@@ -92,10 +111,94 @@ async fn main() -> Result<(), Error> {
         let actor = format!("{:15} ({:2})", user.actor.display_login.green(), actor_link);
         let repo = format!("{:20} ({:>2})", user.repo.name.red(), repo_link);
 
-        println!(" => {:<40} | {:<10} | {:<60} ", &actor, event.blue(), &repo,)
+        println!(" => {:<40} {:<10} {:<60} ", &actor, event.blue(), &repo);
+
+        let response = reqwest::Client::new()
+            .get(&user.repo.url)
+            .header(USER_AGENT, "github_dashboard_feed")
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            let repo_details: RepoDetails = response.json().await?;
+
+            println!("{:>70}", &repo_details.description.expect(""));
+            println!("{:>70}", format!(" {}", &repo_details.stargazers_count));
+            println!("{:>70}", &repo_details.language.expect(""));
+        }
     }
 
     println!("{:=<70}", "");
 
     Ok(())
+}*/
+
+use std::io::{self, stdout};
+
+use crossterm::{
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{prelude::*, widgets::*};
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    print!("\x1B[2J\x1B[1;1H"); // clear screen
+
+    Terminal::new(CrosstermBackend::new(stdout()))?.draw(ui)?;
+
+    disable_raw_mode()?;
+    Ok(())
+}
+
+fn ui(frame: &mut Frame) {
+    let main_layout = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Length(1),
+            Constraint::Percentage(100),
+            Constraint::Length(1),
+        ],
+    ).split(frame.size());
+
+    frame.render_widget(
+        Block::new().borders(Borders::TOP).title("GitHub Dashboard Feed"),
+        main_layout[0],
+    );
+
+    frame.render_widget(
+        Block::new().borders(Borders::TOP),
+        main_layout[2],
+    );
+
+    let row_container = Layout::new(
+        Direction::Vertical,
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].iter().map(|_c| Constraint::Fill(1)),
+    ).split(main_layout[1]);
+
+    for i in 0..10 {
+        let single_row = Layout::new(
+            Direction::Horizontal,
+            [Constraint::Percentage(100)],
+        ).split(row_container[i]);
+
+        let span1 = Span::raw("Hello ");
+
+        let span2 = Span::styled(
+            "World",
+            Style::new()
+                .fg(Color::Green)
+                .bg(Color::White)
+                .add_modifier(Modifier::BOLD),
+        );
+
+        let span3 = Span::raw("!");
+
+        let text: Text = Text::from(vec![Line::from(vec![span1, span2, span3])]);
+
+        frame.render_widget(
+            Paragraph::new(text)
+                .block(Block::new().borders(Borders::ALL).title("Title Bar")),
+            single_row[0],
+        );
+    }
 }
